4c63f8810658fdfb8c03af0db1ad39308badaca8
Felix <felixrunye@gmail.com>
2024-03-05 13:59:38 +0300

    ui improvement

--- components/Recipe/Recipe.tsx
+++ components/Recipe/Recipe.tsx
@@ -7,10 +7,11 @@ import React from 'react'
 const RecipeComponent = ({title, description, image, ingredients, activeState}: Recipe) => {
   return (
     <div className='flex flex-col gap-3'>
-       <div className=''>
-        <h3 className='text-center'>{title}</h3>
-            <p>{description}</p>
-            <Image src={image} alt={title} width={300} height={300} className='rounded-md' />
+       <div className='flex flex-col justify-center items-center'>
+        <h2 className='text-center uppercase font-bold'>{title}</h2>
+        <span className='border w-40 bg-red-400 h-1 m-2'></span>
+            <p className='text-center'>{description}</p>
+            <Image src={image} alt={title} width={300} height={300} className='rounded-md justify-center items-center' />
             {
                 activeState &&
                 ingredients.map((ingredient, index) => (
