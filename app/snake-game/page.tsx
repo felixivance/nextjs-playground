26862249cae995630fcbe7410e17ced546f0573a
Felix <felixrunye@gmail.com>
2024-05-27 12:25:07 +0300

    updated grid display

--- app/snake-game/page.tsx
+++ app/snake-game/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+
 import React from "react";
+
+const gridSize = 20;
+
+type point = {
+  x: number;
+  y: number;
+};
 
 const SnakeGame = () => {
   return (
     <div className="flex min-h-screen flex-col items-center p-24">
-      SnakeGame
+      {
+        // Grid
+        Array.from({ length: gridSize }).map((_, y) => (
+          <div key={y} className="flex">
+            {Array.from({ length: gridSize }).map((_, x) => (
+              <div key={x} className="w-6 h-6 border border-gray-200"></div>
+            ))}
+          </div>
+        ))
+      }
     </div>
   );
 };
