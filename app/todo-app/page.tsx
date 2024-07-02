c31457a48496adf21302247975987cab56ab7376
Felix <felixrunye@gmail.com>
2024-07-02 11:59:28 +0300

    models creation on todo app

---
+++ app/todo-app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+export const metadata = {
+  title: "Felix Playground - Todo App",
+  description: "Todo sample application, with edit, create, update",
+};
+
+const TodoApp = (props: Props) => {
+  return <div>TodoApp</div>;
+};
+
+export default TodoApp;
---
+++ lib/mongo-connect.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectMongoDB = async()=> {
+    try{
+        if(process.env.MONGO_DB_URI){
+            await mongoose.connect(process.env.MONGO_DB_URI);
+            console.log("connected");
+        }
+       console.log("mongo db uri not found")
+    }catch(error){
+        console.log(error);
+        throw new Error("Error while connecting");
+    }
+}
